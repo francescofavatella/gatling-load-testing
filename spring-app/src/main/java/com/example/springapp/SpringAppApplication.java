4a55c2b25f7260d489bdfb8c557485ef3b48dc07
Francesco Favatella <34793940+francescofavatella@users.noreply.github.com>
2021-01-23 16:02:00 +0100

    Dockerise spring app

--- spring-app/src/main/java/com/example/springapp/SpringAppApplication.java
+++ spring-app/src/main/java/com/example/springapp/SpringAppApplication.java
@@ -2,12 +2,21 @@ package com.example.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class SpringAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAppApplication.class, args);
-	}
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppApplication.class, args);
+    }
 
 }
